625d85cd68d5c7d96e02de0275e7b622ec3defeb
CadhlaRoberts <cadhlar@gmail.com>
2019-06-06 10:54:15 +1000

    UI Elements
    Implement Menu and UI functionality

---
+++ Hell Incorporated/Assets/Scripts/UI/Menu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void playGame()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    // Update is called once per frame
+    public void quitApplication()
+    {
+        quitApplication();
+    }
+
+    public void quitGame()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
