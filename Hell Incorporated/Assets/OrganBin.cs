21064decec0468a9a92f9412b347eefc6a0a3bd3
Tobenaii <akuapengin42@gmail.com>
2019-06-19 15:05:36 +1000

    Game almost done

---
+++ Hell Incorporated/Assets/OrganBin.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrganBin : Interactable
+{
+    [SerializeField]
+    private List<GameObjectPool> m_organPools = new List<GameObjectPool>();
+    private Pickup m_curOrgan;
+    
+
+    public override void OnClick(GameObject hand)
+    {
+        int index = Random.Range(0, m_organPools.Count);
+        Pickup organ = m_organPools[index].GetObject().GetComponent<Pickup>();
+        organ.transform.SetParent(null);
+        organ.transform.position = hand.transform.position + hand.transform.forward;
+        m_curOrgan = organ;
+        organ.OnClick(hand);
+    }
+
+    public override void OnRelease(GameObject hand)
+    {
+        m_curOrgan.OnRelease(hand);
+    }
+
+    public override void OnHeld(GameObject hand)
+    {
+        m_curOrgan.OnHeld(hand);
+    }
+}
